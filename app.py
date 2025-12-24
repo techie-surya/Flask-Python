f9af4118b16c6881a63fbe400220262380853943
Techie-Surya <surya14dash@gmail.com>
2025-12-24 14:55:40 +0530

    Final Todo App Project

--- app.py
+++ app.py
@@ -33,11 +33,20 @@ def show():
     print(allTodo)
     return "this is product page."
 
-@app.route("/update")
-def update():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return "this is product page."
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
 
 @app.route("/delete/<int:sno>")
 def delete(sno):
@@ -45,6 +54,10 @@ def delete(sno):
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
